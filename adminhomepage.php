f64bc6b4e71839a3026ef1908eb880493a9bcb1e
marjanajenia <63357966+marjanajenia@users.noreply.github.com>
2021-03-13 22:48:49 +0600

    home page

---
+++ adminhomepage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin home page</title>
+</head>
+<body>
+	<h1>Welcome Admin</h1>
+
+	<h3><a href="addStaff.php">Add Staff</a></h3>
+	<h3><a href="addReceptionist.php">Add Receptionist</a></h3>
+	<h3><a href="deleteStaff.php">Delete Staff</a></h3>
+	<h3><a href="deleteReceptionist.php">Delete Receptionist</a></h3>
+	<h3><a href="changepassword.php">Change Password</a></h3>
+	<h3><a href="setsalary.php">Set Salary</a></h3>
+	<h3><a href="complainbox.php">Check Complain Box</a></h3>
+	<h3><a href="staffList.php">Staff List</a></h3>
+	<h3><a href="adminlogout.php">Logout</a></h3>
+	
+	
+
+
+
+</body>
+</html>
