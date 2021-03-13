732ab0dd20589aa0ad1e5858f28c7961cbb7c607
marjanajenia <63357966+marjanajenia@users.noreply.github.com>
2021-03-13 22:55:16 +0600

    set salary

---
+++ setsalary.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>set salary</title>
+</head>
+<body>
+	<h1>SET SALARY</h1>
+
+	<h3><a href="staffsalary.php.">Staff salry</a></h3>
+	<h3><a href="receptionistsalary.php">Receptionist salary</a></h3>
+
+</body>
+</html>
