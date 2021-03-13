f413abe2dad40d488550fe2405b19be8b36638e9
marjanajenia <63357966+marjanajenia@users.noreply.github.com>
2021-03-13 22:37:57 +0600

    user type

---
+++ usertype.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>user type</title>
+</head>
+<body>
+	<h1>User Type</h1>
+	<h2><a href="adminlogin.php">Admin</a></h2>
+	<h2><a href="">Staff</a></h2>
+	<h2><a href="">Receptionist</a></h2>
+	<h2><a href="">Doner</a></h2>
+	
+	
+
+
+
+</body>
+</html>
