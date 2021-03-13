8ca3020ab9b86e26eaa06546e705684d4086e9a5
marjanajenia <63357966+marjanajenia@users.noreply.github.com>
2021-03-13 23:00:20 +0600

    delete staff

---
+++ deleteStaff.php
@@ -0,0 +1,73 @@
+<?php 
+   
+      $usernameerr ="";
+
+      if($_SERVER['REQUEST_METHOD'] == "POST") {
+
+         if(empty($_POST['username'])) {                    
+         $usernameerr = "Please Fill up the Username!";
+         }
+         else
+         {
+         	$username = $_POST['username'];
+         	$log = fopen("R.txt", "r");
+                    $data = fread($log, filesize("R.txt"));
+                    fclose($log);
+                    
+                    $data_filter = explode("\n", $data);
+
+                    for($i = 0; $i< count($data_filter)-1; $i++)
+                    {
+
+                     $json_decode = json_decode($data_filter[$i], true);
+
+                      if($json_decode['username'] == $username) 
+                        {
+                            
+         	              $fc=file("R.txt");
+         	              $f = fopen("R.txt", "w");       
+                          foreach($fc as $line)
+                           {
+            	              if(!strstr($line,$username)){
+            		             fputs($f,$line);
+            	                 }
+
+                           }  
+              
+                           fclose($f);
+                           header('Location:adminhomepage.php');
+                    
+                        }
+                     }
+                      
+                      
+                    	echo "username not match! try again";
+                          
+                          
+         }
+        }
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>delete staff</title>
+</head>
+<body>
+	<h1>Delete Staff</h1>
+	<form action="deleteStaff.php" method="POST">
+		<table>
+		<tr> 
+			<td>Enter Staff username:</td>
+			<td><input type="text" name="username" id="username"></td>
+			<?php echo $usernameerr; ?>
+		</tr>
+		<tr>
+			<td><input type="submit" value="delete"></td>
+		</tr>
+		</table>
+	</form>
+
+</body>
+</html>
